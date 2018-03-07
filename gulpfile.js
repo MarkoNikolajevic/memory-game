fc5477f5ab2ef5da7aa8668789d7a8011167e0e1
Marko Nikolajevic <mrknikolajevic@gmail.com>
2018-03-07 22:22:23 +0100

    Add eslint

--- gulpfile.js
+++ gulpfile.js
@@ -11,52 +11,52 @@ require("babel-register");
 
 // Compile sass and minify
 gulp.task("sass", () =>
-  gulp.src("src/assets/style/sass/**/*.sass")
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      outputStyle: "compressed"
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      browsers: "last 5 versions"
-    }))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("dist/assets/style/css"))
-    .pipe(browserSync.stream())
+	gulp.src("src/assets/style/sass/**/*.sass")
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: "compressed"
+		}))
+		.on("error", sass.logError)
+		.pipe(autoprefixer({
+			browsers: "last 5 versions"
+		}))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(sourcemaps.write("./"))
+		.pipe(gulp.dest("dist/assets/style/css"))
+		.pipe(browserSync.stream())
 );
 
 // Minify js files
 gulp.task("minifyJs", (cb) =>
-  pump([
-    gulp.src("src/assets/js/*.js"),
-    sourcemaps.init(),
-    babel({
-      presets: ["env"]
-    }),
-    uglify(),
-    rename({
-      suffix: ".min"
-    }),
-    sourcemaps.write("."),
-    gulp.dest("dist/assets/js"),
-    browserSync.stream()
-  ], cb)
+	pump([
+		gulp.src("src/assets/js/*.js"),
+		sourcemaps.init(),
+		babel({
+			presets: ["env"]
+		}),
+		uglify(),
+		rename({
+			suffix: ".min"
+		}),
+		sourcemaps.write("."),
+		gulp.dest("dist/assets/js"),
+		browserSync.stream()
+	], cb)
 );
 
 // Browser Sync
 
 gulp.task("serve", ["sass", "minifyJs"], function() {
-  browserSync.init({
-    server: {
-      index: "index.html"
-    }
-  });
-  gulp.watch("src/assets/style/sass/**/*.sass", ["sass"]);
-  gulp.watch("src/assets/js/*.js", ["minifyJs"]);
-  gulp.watch("index.html").on("change", browserSync.reload);
+	browserSync.init({
+		server: {
+			index: "index.html"
+		}
+	});
+	gulp.watch("src/assets/style/sass/**/*.sass", ["sass"]);
+	gulp.watch("src/assets/js/*.js", ["minifyJs"]);
+	gulp.watch("index.html").on("change", browserSync.reload);
 });
 
 // Default
