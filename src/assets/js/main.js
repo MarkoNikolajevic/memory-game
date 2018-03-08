eabe1494633156e6ed3d6a0948a67d1e9061366b
Marko Nikolajevic <mrknikolajevic@gmail.com>
2018-03-08 11:17:32 +0100

    Add shuffle function

---
+++ src/assets/js/main.js
@@ -0,0 +1,18 @@
+// Create an array of all cards
+let cards = document.querySelectorAll(".card");
+cards = Array.from(cards);
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	let currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
+shuffle(cards);
